7c4f0c646dd86a56d1ac8d560445bd30132635e4
Jenn Cox <jenncox@Jenns-MacBook-Air.local>
2023-12-15 16:02:18 -0500

    Replaced the reducer slice code with the previously written code.

--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -1,43 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit';
+import {
+  AUTH,
+  DELETE_USER,
+  LOGOUT,
+  USER_INFO,
+  USER_UPDATE_NAME,
+} from '@/constants/actions';
 
-const initialState = { authData: null };
-
-const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    setAuth: (state, action) => {
-      localStorage.setItem('profile', JSON.stringify({ ...action.payload }));
-      state.authData = action.payload;
-      state.errors = null;
-    },
-    logout: (state) => {
+// handle user actions
+const authReducer = (state = { authData: null }, action) => {
+  switch (action.type) {
+    case AUTH:
+      localStorage.setItem('profile', JSON.stringify({ ...action?.data }));
+      return { ...state, authData: action.data, errors: null };
+    case LOGOUT:
       localStorage.removeItem('profile');
-      state.authData = null;
-      state.errors = null;
-    },
-    setUserInfo: (state, action) => {
+      return { ...state, authData: null, errors: null };
+    case USER_INFO:
       const userObject = JSON.parse(localStorage.getItem('profile'));
-      userObject.result.coins = action.payload.coins;
+      userObject.result.coins = action?.data.coins;
       localStorage.setItem('profile', JSON.stringify(userObject));
-      state.authData = action.payload;
-      state.errors = null;
-    },
-    updateUser: (state, action) => {
+      return { ...state, authData: action.data, errors: null };
+    case USER_UPDATE_NAME:
       const userObjectNewName = JSON.parse(localStorage.getItem('profile'));
-      userObjectNewName.result.name = action.payload.name;
+      userObjectNewName.result.name = action?.data.name;
       localStorage.setItem('profile', JSON.stringify(userObjectNewName));
-      state.authData = action.payload;
-      state.errors = null;
-    },
-    deleteUser: (state) => {
+      return { ...state, authData: action.data, errors: null };
+    case DELETE_USER:
       localStorage.removeItem('profile');
-      state.authData = null;
-      state.errors = null;
-    },
-  },
-});
+      return { ...state, authData: null, errors: null };
+    default:
+      return state;
+  }
+};
 
-export const { setAuth, logout, setUserInfo, updateUser, deleteUser } = authSlice.actions;
-
-export default authSlice.reducer;
+export default authReducer;
